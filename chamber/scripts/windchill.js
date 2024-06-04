f4c8975b4c2b196f181613e58e5965bcce1354c2
Ben Davis <bendavis493@gmail.com>
2024-06-03 20:39:06 -0600

    added rounding

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,5 +4,5 @@ const windChill = document.getElementById('wind-chill')
 
 // Only apply windchill if temp and windspeed are in these ranges.
 if (degrees <= 50 && windSpeed > 3) {
-    windChill.textContent = 35.74 + (degrees * .6215) - (35.75 * Math.pow(windSpeed,.16)) + (.4275 * degrees * Math.pow(windSpeed,.16))
+    windChill.textContent = Math.round(100 * (35.74 + (degrees * .6215) - (35.75 * Math.pow(windSpeed,.16)) + (.4275 * degrees * Math.pow(windSpeed,.16)))) / 100
 }
