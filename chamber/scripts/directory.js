c2bb4753b2f0d634cf5ac89eee870ba976e5fc37
Ben Davis <bendavis493@gmail.com>
2024-06-20 14:33:41 -0600

    directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,4 +6,27 @@ async function getData() {
 }
 
 const data = await getData();
-console.log(data.json());
+displayMembers(await data.json());
+
+const viewButton = document.getElementById("view-mode-button");
+viewButton.addEventListener('click',() => {
+    cardArea.classList.toggle('grid');
+    viewButton.textContent = cardArea.classList.contains('grid') ? 'View as List' : 'View as Grid';
+})
+
+function displayMembers(members) {
+    cardArea.innerHTML = members.map(info => 
+            `
+            <section class="card">
+                <h2>${info.name}</h2>
+                <p>${info.description}</p>
+                <p>${info.membership_level} Member</p>
+                <p>Founded ${info.founded_year}</p>
+                <img src="${info.image}" alt="${info.name}" loading="lazy" height="400" >
+                <p>${info.address}</p>
+                <p>${info.phone}</p>
+                <p>Visit us at ${info.website}</p>
+            </section>
+            `
+    ).join('');
+}
