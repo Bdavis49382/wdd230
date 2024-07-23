8998652cd18da94ae3d58dca13bec0150fcbee92
Ben Davis <bendavis493@gmail.com>
2024-07-23 15:32:14 -0600

    fixed rounding

--- storefront/scripts/products.js
+++ storefront/scripts/products.js
@@ -5,7 +5,7 @@ document.getElementById('product-cards').innerHTML = products.map(product =>
             <h2>${product.name}</h2>
             <p>${product.description}</p>
             <img src="${product.image}" alt="${product.name}" loading="lazy" height=200>
-            <p>$${product.price}</p>
+            <p>$${product.price.toFixed(2)}</p>
             <a href="./order.html?product=${product.sku}" class="link">Order Now!</a>
         </div>`
         ).join("");
